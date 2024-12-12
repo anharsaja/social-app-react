a4cacb904c4979621d95e4b4abe7c132f0b7a300
anharsaja <anharmuk12@gmail.com>
2024-12-12 21:31:01 +0700

    update

--- components/ScreenWrapper.jsx
+++ components/ScreenWrapper.jsx
@@ -1,12 +1,11 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-const ScreenWrapper = ({ children, bg }) => {
-
+const ScreenWrapper = ({children, bg}) => {
+  
   const {top} = useSafeAreaInsets();
   const paddingTop = top>0? top+5: 30;
-
   return (
     <View style={{flex: 1, paddingTop, backgroundColor: bg}}>
       {
@@ -16,4 +15,4 @@ const ScreenWrapper = ({ children, bg }) => {
   )
 }
 
-export default ScreenWrapper;
+export default ScreenWrapper
