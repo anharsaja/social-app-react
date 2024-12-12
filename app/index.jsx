8276ce02c736cfa7d3afe8cf42598206f400d0da
anharsaja <anharmuk12@gmail.com>
2024-12-12 21:31:01 +0700

    update

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useRouter } from 'expo-router';
+import ScreenWrapper from "../components/ScreenWrapper"
 
-export default function index() {
+const index = () => {
+    const router = useRouter();
     return (
-        <View>
+        <ScreenWrapper>
             <Text>index</Text>
-        </View>
+            <Button title="welcome" onPress={() => router.push('welcome')} />
+        </ScreenWrapper>
     )
 }
+
+export default index
