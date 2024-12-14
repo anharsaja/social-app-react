6baa48fdd1d08b84602258cf93b6bd0ec216a3e1
anharsaja <anharmuk12@gmail.com>
2024-12-14 20:24:26 +0700

    feat: login button

--- app/welcome.jsx
+++ app/welcome.jsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View, ViewBase } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View, ViewBase } from "react-native";
 import React from "react";
 import ScreenWrapper from '../components/ScreenWrapper'
 import { StatusBar } from "expo-status-bar";
@@ -26,9 +26,19 @@ const Welcome = () => {
         <View style={styles.footer}>
           <Button
             title="Getting Started"
-            buttonStyle={{marginHorizontal: wp(3)}}
-            onPress={()=>{}}
+            buttonStyle={{ marginHorizontal: wp(3) }}
+            onPress={() => { }}
           />
+          <View style={styles.bottomTextContainer}>
+            <Text style={styles.loginText}>
+              Already have an account!
+            </Text>
+            <Pressable>
+              <Text style={[styles.loginText, { color: theme.colors.primaryDark }]}>
+                Login
+              </Text>
+            </Pressable>
+          </View>
         </View>
       </View>
     </ScreenWrapper>
@@ -40,31 +50,42 @@ export default Welcome;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    backgroundColor: 'white',
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "white",
     paddingHorizontal: wp(4),
   },
 
   welcomeImage: {
     height: hp(30),
     width: wp(100),
-    alignSelf: 'center'
+    alignSelf: "center"
   },
   title: {
     color: theme.colors.text,
     fontSize: hp(4),
-    textAlign: 'center',
+    textAlign: "center",
     fontWeight: theme.fonts.extraBold
   },
   punchline: {
-    textAlign: 'center',
+    textAlign: "center",
     paddingHorizontal: wp(10),
     fontSize: hp(1.7),
     color: theme.colors.text
   },
   footer: {
     gap: 30,
-    width: '100%'
+    width: "100%"
+  },
+  bottomTextContainer: {
+    flexDirection: 'row',
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5
+  },
+  loginText: {
+    textAlign: "center",
+    color: theme.colors.text,
+    fontSize: hp(1.8)
   }
 });
