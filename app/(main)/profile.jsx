198259c6b24b1bdb2a4ec981eda7b9ec3d382b20
anharsaja <anharmuk12@gmail.com>
2024-12-17 01:06:07 +0700

    feat: button route

--- app/(main)/profile.jsx
+++ app/(main)/profile.jsx
@@ -1,10 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import ScreenWrapper from '../../components/ScreenWrapper'
+import { useAuth } from '../../contexts/AuthContext'
+import { useRouter } from 'expo-router'
+import Header from '../../components/Header'
+import { wp } from '../../helpers/common'
 
 const Profile = () => {
+    const {user, setAuth} = useAuth();
+    const router = useRouter();
+    
     return (
-        <View>
-            <Text>Profile</Text>
+        <ScreenWrapper bg="white">
+            <UserHeader user={user} router={router}/>
+        </ScreenWrapper>
+    )
+}
+
+const UserHeader = ({user, router}) => {
+    return (
+        <View style={{flex: 1, backgroundColor: "white", paddingHorizontal: wp(4)}}>
+            <View>
+                <Header title="Profile" showBackButton={true}/>
+            </View>
         </View>
     )
 }
--- app/signUp.jsx
+++ app/signUp.jsx
@@ -3,13 +3,13 @@ import React, { useRef, useState } from 'react'
 import ScreenWrapper from '../components/ScreenWrapper'
 import { theme } from '../constants/theme'
 import { StatusBar } from 'expo-status-bar'
-import BackButton from '../components/BackButton'
 import { useRouter } from 'expo-router'
 import { hp, wp } from '../helpers/common'
 import Input from '../components/Input'
 import Button from '../components/Button'
 import Icon from '../assets/icons'
 import { supabase } from '../lib/subapase'
+import WelcomeButton from '../components/WelcomeButton'
 
 const SignUp = () => {
     const router = useRouter();
@@ -53,7 +53,7 @@ const SignUp = () => {
         <ScreenWrapper bg="white">
             <StatusBar style="dark" />
             <View style={styles.container}>
-                <BackButton router={router} />
+                <WelcomeButton router={router} />
 
                 {/* welcome */}
                 <View>
