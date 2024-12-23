e2465d7363093e089319586cbe9e23c23c54fc73
anharsaja <anharmuk12@gmail.com>
2024-12-24 03:11:41 +0700

    ✨ feat: add feature (posts)

--- app/(main)/newPost.jsx
+++ app/(main)/newPost.jsx
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Alert, Pressable, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useRef, useState } from 'react'
 import ScreenWrapper from '../../components/ScreenWrapper'
 import Header from '../../components/Header'
@@ -8,6 +8,13 @@ import { useAuth } from '../../contexts/AuthContext'
 import Avatar from '../../components/Avatar'
 import RichTextEditor from '../../components/RichTextEditor'
 import { useRouter } from 'expo-router'
+import Icon from '../../assets/icons'
+import Button from '../../components/Button'
+import * as ImagePicker from 'expo-image-picker'
+import { getSupabaseFileUrl } from '../../services/imageService'
+import { Image } from 'expo-image'
+import { Video } from 'expo-av'
+import { createOrUpdatePost } from '../../services/postService'
 
 const NewPost = () => {
     const { user } = useAuth();
@@ -17,6 +24,92 @@ const NewPost = () => {
     const [loading, setLoading] = useState(false);
     const [file, setFile] = useState(file);
 
+    const onPick = async (isImage) => {
+        let mediaConfig = {
+            mediaTypes: 'images',
+            allowsEditing: true,
+            aspect: [1, 1],
+            // quality: 1,
+            quality: 0.7
+        }
+        if (!isImage) {
+            mediaConfig = {
+                mediaTypes: 'videos',
+                allowsEditing: true,
+                aspect: [1, 1],
+                // quality: 1,
+                quality: 0.7
+            }
+        }
+        let result = await ImagePicker.launchImageLibraryAsync(mediaConfig);
+
+        if (!result.canceled) {
+            // console.log(result.assets[0])
+            setFile(result.assets[0]);
+        }
+    }
+
+    const isLocalFile = file => {
+        if(!file) return null;
+        if(typeof file == 'object') return true;
+
+        return false
+    }
+
+    const getFileType = file => {
+        if (!file) return null;
+        if(isLocalFile(file)){
+            return file.type;
+        }
+
+        // check image or videos for remote file
+        if(file.includes('postImages')){
+            return 'image'
+        }
+        return 'video'
+    }
+
+    const getFileUri = file => {
+        if (!file) return null;
+        if (isLocalFile(file)){
+            return file.uri;
+        }
+
+        return getSupabaseFileUrl(file)?.uri;
+    }
+    
+    const onSubmit = async () => {
+        // console.log('body: ', bodyRef.current)
+        // console.log('file: ', file)
+        if (!bodyRef.current && !file){
+            Alert.alert('Postingan', "Isinen sek cok sakdurunge ngirim!!")
+            return;
+        }
+        
+        let data = {
+            file,
+            body: bodyRef.current,
+            userId: user?.id
+        }
+
+        // create post
+        setLoading(true);
+        let res = await createOrUpdatePost(data);
+        setLoading(false);
+        // console.log('post res: ', res);
+        if(res.success){
+            setFile(null);
+            bodyRef.current = '';
+            editorRef.current?.setContentHTML('');
+            router.back();
+        }else{
+            Alert.alert("post", res.msg);
+        }
+
+    }
+
+    // console.log('file uri: ', getFileUri(file));
+    // console.log('file type: ', getFileType(file));
     return (
         <ScreenWrapper bg={'white'}>
             <View style={styles.container}>
@@ -41,9 +134,54 @@ const NewPost = () => {
                         </View>
                     </View>
                     <View style={styles.textEditor}>
-                        <RichTextEditor editorRef={editorRef} onChange={body => bodyRef.current = body}/>
+                        <RichTextEditor editorRef={editorRef} onChange={body => bodyRef.current = body} />
+                    </View>
+
+                    {
+                        file && (
+                            <View style={styles.file}>
+                                {
+                                    getFileType(file) == 'video' ? (
+                                        <Video 
+                                            style={{flex: 1}}
+                                            source={{
+                                                uri: getFileUri(file)
+                                            }}
+                                            useNativeControls
+                                            resizeMode='cover'
+                                            isLooping
+                                        />
+                                    ):(
+                                        <Image source={{uri: getFileUri(file)}} style={{flex: 1, resizeMode: 'cover'}}/>
+                                    )
+                                }
+
+                                <Pressable style={styles.closeIcon} onPress={() => setFile(null)}>
+                                    <Icon name="delete" size={20} color="white"/>
+                                </Pressable>
+                            </View>
+                        )
+                    }
+
+                    <View style={styles.media}>
+                        <Text style={styles.addImageText}>Add to your post</Text>
+                        <View style={styles.mediaIcons}>
+                            <TouchableOpacity onPress={() => onPick(true)}>
+                                <Icon name="image" size={30} color={theme.colors.dark} />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => onPick(false)}>
+                                <Icon name="video" size={33} color={theme.colors.dark} />
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </ScrollView>
+                <Button
+                    buttonStyle={{ height: hp(6.2) }}
+                    title='Post'
+                    loading={loading}
+                    hasShadow={false}
+                    onPress={onSubmit}
+                />
             </View>
         </ScreenWrapper>
     )
@@ -130,8 +268,12 @@ const styles = StyleSheet.create({
     },
     closeIcon: {
         position: 'absolute',
-        top: '10',
-        right: '10',
+        top: 10,
+        right: 10,
+        padding: 7,
+        borderRadius: 50,
+        backgroundColor: 'rgba(255,0,0,0.6)'
+
         // shadowColor: theme.colors.textLight,
         // shadowOffset: {width: 0, height: 3},
         // shadowOpacity: 0.6,
