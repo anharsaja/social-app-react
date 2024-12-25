fc4ef354997c37836b6c2a789adceb6131aa4334
anharsaja <anharmuk12@gmail.com>
2024-12-25 12:32:58 +0700

    ✨ feat: home page post

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,168 +1,181 @@
-import { Alert, FlatList, Pressable, StatusBar, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import ScreenWrapper from '../../components/ScreenWrapper'
-import { useRouter } from 'expo-router'
-import { useAuth } from '../../contexts/AuthContext'
-import { supabase } from '../../lib/subapase'
-import { hp, wp } from '../../helpers/common'
-import { theme } from '../../constants/theme'
-import Icon from '../../assets/icons'
-import Avatar from '../../components/Avatar'
-import Button from '../../components/Button'
-import { fetchPosts } from '../../services/postService'
-import PostCard from '../../components/PostCard'
+import {
+  Alert,
+  FlatList,
+  Pressable,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import ScreenWrapper from "../../components/ScreenWrapper";
+import { useRouter } from "expo-router";
+import { useAuth } from "../../contexts/AuthContext";
+import { supabase } from "../../lib/subapase";
+import { hp, wp } from "../../helpers/common";
+import { theme } from "../../constants/theme";
+import Icon from "../../assets/icons";
+import Avatar from "../../components/Avatar";
+import Button from "../../components/Button";
+import { fetchPosts } from "../../services/postService";
+import PostCard from "../../components/PostCard";
+import Loading from "../../components/Loading";
 
 var limit = 0;
 const Home = () => {
+  const { user, setAuth } = useAuth();
+  const router = useRouter();
 
-    const { user, setAuth } = useAuth();
-    const router = useRouter();
+  const [posts, setPosts] = useState([]);
 
-    const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    getPosts();
+  }, []);
 
+  const getPosts = async () => {
+    // call the API here
+    limit = limit + 10;
 
-    useEffect(() => {
-        getPosts();
-    }, [])
-
-    const getPosts = async () => {
-        // call the API here
-        limit = limit + 10;
-
-        console.log('fetching posts', limit);
-        let res = await fetchPosts(limit);
-        if(res.success){
-            setPosts(res.data);
-        }
-        // console.log('got posts result: ', res);
-        // console.log('user: ', res.data[0].user)
+    console.log("fetching posts", limit);
+    let res = await fetchPosts(limit);
+    if (res.success) {
+      setPosts(res.data);
     }
-    
-    // useEffect(() => {
-    //     console.log('User logged in:', user);
-    // }, [user]);
-    
+    // console.log('got posts result: ', res);
+    // console.log('user: ', res.data[0].user)
+  };
 
-    // const onLogout = async () => {
-    //     // setAuth(null);
-    //     const { error } = await supabase.auth.signOut();
-    //     if (error) {
-    //         Alert.alert('Sign out', "Error sign out");
-    //     }
-    // }
+  // useEffect(() => {
+  //     console.log('User logged in:', user);
+  // }, [user]);
 
-    return (
-        <ScreenWrapper bg="white">
-            <View style={styles.container}>
-                {/* header */}
-                <View style={styles.header}>
-                    <Text style={styles.title}>Instagram</Text>
-                    <View style={styles.icons}>
-                        <Pressable onPress={() => router.push('notifications')}>
-                            <Icon name="heart" size={hp(3.2)} strokeWidth={2} color={theme.colors.text} />
-                        </Pressable>
-                        <Pressable onPress={() => router.push('newPost')}>
-                            <Icon name="plus" size={hp(3.2)} strokeWidth={2} color={theme.colors.text} />
-                        </Pressable>
-                        <Pressable onPress={() => router.push('profile')}>
-                            <Avatar 
-                                uri={user?.image}
-                                size={hp(4.3)}
-                                rounded={theme.radius.sm}
-                                style={{borderWidth: 2}}
-                            />
-                        </Pressable>
-                    </View>
-                </View>
+  // const onLogout = async () => {
+  //     // setAuth(null);
+  //     const { error } = await supabase.auth.signOut();
+  //     if (error) {
+  //         Alert.alert('Sign out', "Error sign out");
+  //     }
+  // }
 
-                {/* Posts */}
-                <FlatList
-                    data={posts}
-                    showsVerticalScrollIndicator={false}
-                    contentContainerStyle={styles.listStyle}
-                    keyExtractor={item => item.id.toString()}
-                    renderItem={({item}) => <PostCard
-                        item={item}
-                        currentUser={user}
-                        router={router}
-                    />
-                    }
-                />
+  return (
+    <ScreenWrapper bg="white">
+      <View style={styles.container}>
+        {/* header */}
+        <View style={styles.header}>
+          <Text style={styles.title}>Instagram</Text>
+          <View style={styles.icons}>
+            <Pressable onPress={() => router.push("notifications")}>
+              <Icon
+                name="heart"
+                size={hp(3.2)}
+                strokeWidth={2}
+                color={theme.colors.text}
+              />
+            </Pressable>
+            <Pressable onPress={() => router.push("newPost")}>
+              <Icon
+                name="plus"
+                size={hp(3.2)}
+                strokeWidth={2}
+                color={theme.colors.text}
+              />
+            </Pressable>
+            <Pressable onPress={() => router.push("profile")}>
+              <Avatar
+                uri={user?.image}
+                size={hp(4.3)}
+                rounded={theme.radius.sm}
+                style={{ borderWidth: 2 }}
+              />
+            </Pressable>
+          </View>
+        </View>
 
+        {/* Posts */}
+        <FlatList
+          data={posts}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.listStyle}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) =>
+            <PostCard
+              item={item}
+              currentUser={user}
+              router={router}
+            />
+          }
+
+          ListFooterComponent={(
+            <View style={{ marginVertical: posts.length == 0 ? 300 : 30 }}>
+              <Loading />
             </View>
-            {/* <Button title='logout' onPress={onLogout}/> */}
-            {/* <View style={[styles.footer, {paddingBottom: 15}]}>
-                <Button
-                    title="Log Out"
-                    buttonStyle={{ marginHorizontal: wp(35), backgroundColor: "red" }}
-                    onPress={onLogout}
-                />
-            </View> */}
+          )}
+        />
+      </View>
+    </ScreenWrapper>
+  );
+};
 
-        </ScreenWrapper>
-    )
-}
-
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        // paddingHorizontal: wp(4)
-    },
-    header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        marginBottom: 10,
-        marginHorizontal: wp(4)
-    },
-    title: {
-        color: theme.colors.text,
-        fontSize: hp(3.2),
-        fontWeight: theme.fonts.bold
-    },
-    avatarImage: {
-        height: hp(4.3),
-        width: hp(4.3),
-        borderRadius: theme.radius.sm,
-        borderCurve: 'continuous',
-        borderColor: theme.colors.gray,
-        borderWidth: 3
-    },
-    icons: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: 18
-    },
-    listStyle: {
-        paddingTop: 20,
-        paddingHorizontal: wp(4)
-    },
-    noPosts: {
-        fontSize: hp(2),
-        textAlign: 'center',
-        color: theme.colors.text
-    },
-    pill: {
-        position: 'absolute',
-        right: -10,
-        top: -14,
-        height: hp(2.2),
-        width: hp(2.2),
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 20,
-        backgroundColor: theme.colors.roseLight
-    },
-    pillText: {
-        color: "white",
-        fontSize: hp(1.2),
-        fontWeight: theme.fonts.bold
-    },
-    footer: {
-        gap: 30,
-        width: "100%"
-    },
-})
+  container: {
+    flex: 1,
+    // paddingHorizontal: wp(4)
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+    marginHorizontal: wp(4),
+  },
+  title: {
+    color: theme.colors.text,
+    fontSize: hp(3.2),
+    fontWeight: theme.fonts.bold,
+  },
+  avatarImage: {
+    height: hp(4.3),
+    width: hp(4.3),
+    borderRadius: theme.radius.sm,
+    borderCurve: "continuous",
+    borderColor: theme.colors.gray,
+    borderWidth: 3,
+  },
+  icons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 18,
+  },
+  listStyle: {
+    paddingTop: 20,
+    paddingHorizontal: wp(4),
+  },
+  noPosts: {
+    fontSize: hp(2),
+    textAlign: "center",
+    color: theme.colors.text,
+  },
+  pill: {
+    position: "absolute",
+    right: -10,
+    top: -14,
+    height: hp(2.2),
+    width: hp(2.2),
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+    backgroundColor: theme.colors.roseLight,
+  },
+  pillText: {
+    color: "white",
+    fontSize: hp(1.2),
+    fontWeight: theme.fonts.bold,
+  },
+  footer: {
+    gap: 30,
+    width: "100%",
+  },
+});
